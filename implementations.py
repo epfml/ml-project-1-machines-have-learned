de5a27fcf7938fcc0384321be61736f2263e66eb
Hamza <134798481+hmorchid@users.noreply.github.com>
2023-10-29 20:56:28 +0100

    Update implementations.py

--- implementations.py
+++ implementations.py
@@ -232,6 +232,6 @@ def reg_logistic_regression(y, tx, lambda_, initial_w, max_iters, gamma):
     for n_iter in range(max_iters):
         gradient = compute_gradient_log_reg(y, tx, w) + 2*lambda_*w
         w = w - gamma * gradient
-        losses.append(compute_loss_log_reg(y, tx, w) + lambda_ * np.squeeze(w.T.dot(w)))
+        losses.append(compute_loss_log_reg(y, tx, w))
 
     return w, losses[-1]
